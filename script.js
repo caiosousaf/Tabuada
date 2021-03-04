4ea173152437052118153434bf35bbbfa290cfd0
Caio Sousa <caiosousafernandesferreira@hotmail.com>
2021-03-04 14:41:48 -0300

    Feat: Added the sum function

--- script.js
+++ script.js
@@ -10,15 +10,27 @@ function calcular() {
     } else {
         var n = Number(n1.value)
         var re = ct
-        for (var c = 1; c <= 10; c++) {
+        for (var c = 1; c <= 100; c++) {
             tabuada += n + " x " + c + " = " + n * c + ' \ \ \ \ \ \ '; 
             re.innerText = tabuada
         }
     }
-
-
-
-
-
-
+}
+function somar(){
+    var n1 = document.getElementById("t1")
+    var ct = document.getElementById("resultado")
+    var res = document.getElementById("res")
+    var tabuada = ''
+    res.innerHTML = ('')
+    if (n1.value.length == 0) {
+        window.alert('[ERROR] Voce esqueceu de digitar um número')
+        res.innerHTML = ('[ERROR] Impossivel Somar')
+    } else {
+        var n = Number(n1.value)
+        var re = ct
+        for (var c = 1; c <= 100; c++) {
+            tabuada += n + " + " + c + " = " + (n + c) + ' \ \ \ \ \ \ '; 
+            re.innerText = tabuada
+        }
+    }
 }
