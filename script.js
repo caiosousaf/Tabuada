0b8b2785c06690edda57ceb9f68a01403cbcaf71
Caio Sousa <caiosousafernandesferreira@hotmail.com>
2021-03-07 13:35:23 -0300

    Enter

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function calcular() {
     var n1 = document.getElementById("t1")
-    var ct = document.getElementById("resultado")
+    var ct = document.getElementById("result")
     var res = document.getElementById("res")
+function calculate() {
     var tabuada = ''
     res.innerHTML = ('')
     if (n1.value.length == 0) {
@@ -9,17 +9,13 @@ function calcular() {
         res.innerHTML = ('[ERROR] Impossivel Multiplicar')
     } else {
         var n = Number(n1.value)
-        var re = ct
-        for (var c = 1; c <= 100; c++) {
+        for (var c = 0; c <= 100; c++) {
             tabuada += n + " x " + c + " = " + n * c + ' \ \ \ \ \ \ '; 
-            re.innerText = tabuada
+            ct.innerText = tabuada
         }
     }
 }
-function somar(){
-    var n1 = document.getElementById("t1")
-    var ct = document.getElementById("resultado")
-    var res = document.getElementById("res")
+function Sum(){
     var tabuada = ''
     res.innerHTML = ('')
     if (n1.value.length == 0) {
@@ -27,10 +23,23 @@ function somar(){
         res.innerHTML = ('[ERROR] Impossivel Somar')
     } else {
         var n = Number(n1.value)
-        var re = ct
-        for (var c = 1; c <= 100; c++) {
+        for (var c = 0; c <= 100; c++) {
             tabuada += n + " + " + c + " = " + (n + c) + ' \ \ \ \ \ \ '; 
-            re.innerText = tabuada
+            ct.innerText = tabuada
+        }
+    }
+}
+function Subtraction(){
+    var tabuada = ''
+    res.innerHTML = ('')
+    if (n1.value.length == 0) {
+        window.alert('[ERROR] Voce esqueceu de digitar um número')
+        res.innerHTML = ('[ERROR] Impossivel Subtrair')
+    } else {
+        var n = Number(n1.value)
+        for (var c = 0; c <= 100; c++) {
+            tabuada += n + " - " + c + " = " + (n - c) + ' \ \ \ \ \ \ '; 
+            ct.innerText = tabuada
         }
     }
 }
